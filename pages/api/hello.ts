7a0c759aac8e67ccd351b3bfa5f45c62c87aa144
traam0 <younesmrx@outlook.com>
2023-11-29 14:40:07 +0100

    j

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Resend } from "resend";
 
-type Data = {
-  name: string
-}
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const data = await resend.emails.send({
+      from: "CLEFJIB <no-reply@resend.dev>",
+      to: ["younesmrx@gmail.com"],
+      subject: "Hello world",
+      html: "dmfhdjfndjnf",
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 }
